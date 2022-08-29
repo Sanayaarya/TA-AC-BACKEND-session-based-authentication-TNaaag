b5de958aabad3991055ebc3fb022db448d009436
Sanaya Arya <sanayaarya0123@gmail.com>
2022-08-30 04:21:29 +0530

    Completed

---
+++ block-BNaadt/authentication/routes/users.js
@@ -0,0 +1,39 @@
+router.get('/register', (req, res, next) => {
+  res.render('register');
+});
+
+router.post('/register', (req, res, next) => {
+  User.create(req.body, (err, user) => {
+    if(err) return next(err);
+    res.redirect('/users/login');
+  })
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.post('/login', (req, res, next) => {
+  var { email,password } = req.body;
+  if(!email || !password ) {
+    res.redirect('/users/login')
+  }
+  User.findOne({ email }, (err, user) => {
+    if(err) return next(err);
+    // no user
+    if(!user) {
+      return res.redirect('/users/login');
+    }
+    // compare password
+    user.verifyPassword(password, (err, result) => {
+      if(err) return next(err);
+      if(!result) {
+        return res.redirect('/users/login')
+      }
+      // persist logged in user information
+      req.session.userId = user.id;
+      res.redirect('/users');
+    })
+  })
+})
+module.exports = router;
